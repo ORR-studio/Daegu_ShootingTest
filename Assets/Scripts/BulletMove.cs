a8626737c10912f32590c028c949b8deb9aa1077
Wonseok Park <amurorei2000@gmail.com>
2024-06-26 16:49:36 +0900

    Initial commit

---
+++ Assets/Scripts/BulletMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+    // 위쪽 방향으로 계속 이동하고 싶다.
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // 사용자의 입력(WSAD)을 받아서 그 방향대로 이동하고 싶다.
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
